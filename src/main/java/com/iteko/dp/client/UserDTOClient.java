ef3a281f9916cd4a0271b0ac3686c242c9a6622e
zatifan1 <kuchukov.zakir.mo05@gmail.com>
2019-11-22 18:39:41 +0300

    Добавлены тесты.

--- src/main/java/com/iteko/dp/client/UserDTOClient.java
+++ src/main/java/com/iteko/dp/client/UserDTOClient.java
@@ -9,14 +9,26 @@ import java.util.List;
 
 public interface UserDTOClient {
 
-    @RequestLine("GET /{id}")
+    @RequestLine("PUT /merge")
+    @Headers("Content-Type: application/json")
+    void merge(UserDTO userDTO);
+
+    @RequestLine("GET /find/{id}")
     UserDTO findById(@Param("id") String id);
+
+    @RequestLine("DELETE /remove/{id}")
+    @Headers("Content-Type: application/json")
+    void remove(@Param("id") String id);
 
     @RequestLine("GET")
     List<UserDTO> findAll();
 
-    @RequestLine("POST")
+    @RequestLine("PUT /mergeAll")
     @Headers("Content-Type: application/json")
-    void create(UserDTO userDTO);
+    void mergeAll(List<UserDTO> userDTOList);
+
+    @RequestLine("DELETE /removeAll")
+    @Headers("Content-Type: application/json")
+    void removeAll();
 
 }
--- src/test/java/com/iteko/dp/AppTest.java
+++ src/test/java/com/iteko/dp/AppTest.java
@@ -1,20 +1,76 @@
 package com.iteko.dp;
 
+import com.iteko.dp.client.UserDTOClient;
+import com.iteko.dp.dto.UserDTO;
+import com.iteko.dp.enumeration.Role;
+import lombok.extern.slf4j.Slf4j;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.UUID;
+
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
-import org.junit.Test;
+@Slf4j
+@RunWith(JUnit4.class)
+public class AppTest {
 
-/**
- * Unit test for simple App.
- */
-public class AppTest 
-{
-    /**
-     * Rigorous Test :-)
-     */
-    @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
+    @NotNull
+    private UserDTOClient userDTOClient;
+
+    @NotNull
+    private final UserDTO userDTO = new UserDTO();
+
+    @Before
+    public void setup() {
+        userDTOClient = new UserControllerFeignClientBuilder().getUserDTOClient();
+        userDTO.setUuid(UUID.randomUUID().toString());
+        userDTO.setLogin("test");
+        userDTO.setPassword("test");
+        userDTO.setRoles(Arrays.asList(Role.TEACHER, Role.ADMIN));
     }
+
+    @Test
+    public void getAllUser() {
+        @NotNull List<UserDTO> userDTOS = userDTOClient.findAll();
+        assertTrue(userDTOS.size() > 0);
+        log.info("{}", userDTOS);
+    }
+
+    @Test
+    public void getUserById() {
+        @NotNull final UserDTO userDTO = userDTOClient.findById("client_id");
+        Assert.assertEquals(userDTO.getLogin(), "login");
+    }
+
+    @Test
+    public void createUser() {
+        userDTOClient.merge(userDTO);
+        @Nullable final UserDTO newUserDTO = userDTOClient.findById(userDTO.getUuid());
+        if (newUserDTO != null) Assert.assertEquals(newUserDTO.getLogin(), "test");
+        else Assert.fail("UserDTO is null.");
+    }
+
+    @Test
+    public void removeUser() {
+        userDTOClient.remove(userDTO.getUuid());
+        @Nullable final UserDTO newUserDTO = userDTOClient.findById(userDTO.getUuid());
+        Assert.assertNull(newUserDTO);
+    }
+
+    @Test
+    public void removeAllUser() {
+        userDTOClient.removeAll();
+        @NotNull final List<UserDTO> userDTOS = userDTOClient.findAll();
+        assertEquals(0, userDTOS.size());
+    }
+
 }
