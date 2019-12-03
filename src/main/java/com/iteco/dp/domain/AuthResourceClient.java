1ac93178ae9fc4fb0261ea2e586cf58d899abb4b
zatifan1 <kuchukov.zakir.mo05@gmail.com>
2019-12-03 11:17:19 +0300

    Добавлены тесты для Manager и Candidate, удален Security.

--- src/main/java/com/iteco/dp/domain/AuthResourceClient.java
+++ src/main/java/com/iteco/dp/domain/AuthResourceClient.java
@@ -1,23 +1,12 @@
 package com.iteco.dp.domain;
 
-
 import com.iteco.dp.domain.client.*;
-import com.iteco.dp.domain.interceptor.CookieInterceptor;
 import feign.Feign;
 import feign.jackson.JacksonDecoder;
 import feign.jackson.JacksonEncoder;
 import org.springframework.cloud.openfeign.support.SpringMvcContract;
 
 public class AuthResourceClient {
-
-    public static AuthClient getAuthInstance(final String baseUrl) {
-        return Feign.builder()
-                .contract(new SpringMvcContract())
-                .encoder(new JacksonEncoder())
-                .decoder(new JacksonDecoder())
-                .requestInterceptor(new CookieInterceptor())
-                .target(AuthClient.class, baseUrl);
-    }
 
     public static ManagerClient getManagerInstance(final String baseUrl) {
         return Feign.builder()
@@ -59,4 +48,3 @@ public class AuthResourceClient {
                 .target(TeacherClient.class, baseUrl);
     }
 }
-
