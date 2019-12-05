a6ea412e7a91b117db229610fc4cfe890d963f4b
Sergey Kudryashov <s.kudryashov.msk@gmail.com>
2019-12-05 11:28:55 +0300

    Исправлены типы возвратов

--- src/main/java/com/iteco/dp/domain/client/CourseClient.java
+++ src/main/java/com/iteco/dp/domain/client/CourseClient.java
@@ -1,27 +1,26 @@
 package com.iteco.dp.domain.client;
 
 import com.iteco.dp.domain.dto.CourseDTO;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.Collection;
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "/course")
 public interface CourseClient {
 
     @GetMapping(value = "/findAll")
-    ResponseEntity<Collection<CourseDTO>> findAll();
+    List<CourseDTO> findAll();
 
     @GetMapping(value = "/find/{id}")
-    ResponseEntity<CourseDTO> findById(@PathVariable final String id);
+    CourseDTO findById(@PathVariable final String id);
 
     @PutMapping(value = "/create")
-    ResponseEntity<CourseDTO> create(@RequestBody final CourseDTO courseDTO);
+    CourseDTO create(@RequestBody final CourseDTO courseDTO);
 
     @PutMapping(value = "/update")
-    ResponseEntity<CourseDTO> update(@RequestBody final CourseDTO courseDTO);
+    CourseDTO update(@RequestBody final CourseDTO courseDTO);
 
     @DeleteMapping(value = "/delete/{id}")
-    ResponseEntity delete(@PathVariable final String id);
+    CourseDTO delete(@PathVariable final String id);
 }
--- src/main/java/com/iteco/dp/domain/client/PersonClient.java
+++ src/main/java/com/iteco/dp/domain/client/PersonClient.java
@@ -1,27 +1,26 @@
 package com.iteco.dp.domain.client;
 
 import com.iteco.dp.domain.dto.PersonDTO;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.Collection;
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "/person")
 public interface PersonClient {
 
     @GetMapping("/findAll")
-    ResponseEntity<Collection<PersonDTO>> findAll();
+    List<PersonDTO> findAll();
 
     @GetMapping("/find/{id}")
-    ResponseEntity<PersonDTO> findOne(@PathVariable final String id);
+    PersonDTO findOne(@PathVariable final String id);
 
     @PutMapping("/create")
-    ResponseEntity<PersonDTO> create(@RequestBody final PersonDTO personDTO);
+    PersonDTO create(@RequestBody final PersonDTO personDTO);
 
     @PutMapping("/update")
-    ResponseEntity<PersonDTO> update(@RequestBody final PersonDTO personDTO);
+    PersonDTO update(@RequestBody final PersonDTO personDTO);
 
     @DeleteMapping("/delete/{id}")
-    ResponseEntity delete(@PathVariable final String id);
+    PersonDTO delete(@PathVariable final String id);
 }
