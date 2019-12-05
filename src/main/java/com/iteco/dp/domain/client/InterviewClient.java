449a481e3fbfda3ec8195ec67d988e6d80bd7e20
shipovanatalia <nataliefrance@mail.ru>
2019-12-05 14:07:24 +0300

    В InterviewClient добавлены методы поиска.

--- src/main/java/com/iteco/dp/domain/client/InterviewClient.java
+++ src/main/java/com/iteco/dp/domain/client/InterviewClient.java
@@ -4,6 +4,7 @@ import com.iteco.dp.domain.dto.InterviewDTO;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Date;
 import java.util.List;
 
 @FeignClient("interview")
@@ -24,4 +25,19 @@ public interface InterviewClient {
 
     @DeleteMapping(value = "/delete/{id}")
     void deleteById(@PathVariable("id") String id);
+
+    @GetMapping(value = "/find/{dateOfInterview}", consumes = "application/json")
+    InterviewDTO findByDate(@PathVariable("dateOfInterview") Date dateOfInterview);
+
+    @GetMapping(value = "/find/{teacherId}", consumes = "application/json")
+    InterviewDTO findByTeacher(@PathVariable("teacherId") String teacherId);
+
+    @GetMapping(value = "/find/{candidateId}", consumes = "application/json")
+    InterviewDTO findByCandidate(@PathVariable("candidateId") String candidateId);
+
+    @GetMapping(value = "/find/{status}", consumes = "application/json")
+    InterviewDTO findByStatus(@PathVariable("status") String status);
+
+    @GetMapping(value = "/find/{interviewGrade}", consumes = "application/json")
+    InterviewDTO findByGrade(@PathVariable("interviewGrade") int interviewGrade);
 }
