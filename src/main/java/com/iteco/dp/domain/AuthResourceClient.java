c0dafb74195eb664cfe1f16a60fdd1d7eeff007a
zatifan1 <kuchukov.zakir.mo05@gmail.com>
2019-12-09 14:08:10 +0300

    Изменен ResourceClient, клиенты сделаны Bean.

--- src/main/java/com/iteco/dp/domain/AuthResourceClient.java
+++ src/main/java/com/iteco/dp/domain/AuthResourceClient.java
@@ -2,8 +2,10 @@ package com.iteco.dp.domain;
 
 import com.iteco.dp.domain.client.*;
 import feign.Feign;
+import feign.Logger;
 import feign.jackson.JacksonDecoder;
 import feign.jackson.JacksonEncoder;
+import feign.slf4j.Slf4jLogger;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.openfeign.support.SpringMvcContract;
 import org.springframework.context.annotation.Bean;
@@ -17,111 +19,67 @@ public class AuthResourceClient {
 
     @Bean
     public ManagerClient getManagerClient() {
-        return Feign.builder()
-                .contract(new SpringMvcContract())
-                .encoder(new JacksonEncoder())
-                .decoder(new JacksonDecoder())
-                .target(ManagerClient.class, address);
+        return createClient(ManagerClient.class);
     }
 
     @Bean
     public UserClient getUserClient() {
-        return Feign.builder()
-                .contract(new SpringMvcContract())
-                .encoder(new JacksonEncoder())
-                .decoder(new JacksonDecoder())
-                .target(UserClient.class, address);
+        return createClient(UserClient.class);
     }
 
     @Bean
     public CandidateClient getCandidateClient() {
-        return Feign.builder()
-                .contract(new SpringMvcContract())
-                .encoder(new JacksonEncoder())
-                .decoder(new JacksonDecoder())
-                .target(CandidateClient.class, address);
+        return createClient(CandidateClient.class);
     }
 
     @Bean
     public InterviewClient getInterviewClient() {
-        return Feign.builder()
-                .contract(new SpringMvcContract())
-                .encoder(new JacksonEncoder())
-                .decoder(new JacksonDecoder())
-                .target(InterviewClient.class, address);
+        return createClient(InterviewClient.class);
     }
 
     @Bean
     public TeacherClient getTeacherClient() {
-        return Feign.builder()
-                .contract(new SpringMvcContract())
-                .encoder(new JacksonEncoder())
-                .decoder(new JacksonDecoder())
-                .target(TeacherClient.class, address);
+        return createClient(TeacherClient.class);
     }
 
     @Bean
     public ClassroomClient getClassroomClient() {
-        return Feign.builder()
-                .contract(new SpringMvcContract())
-                .encoder(new JacksonEncoder())
-                .decoder(new JacksonDecoder())
-                .target(ClassroomClient.class, address);
+        return createClient(ClassroomClient.class);
     }
 
     @Bean
     public LessonClient getLessonClient() {
-        return Feign.builder()
-                .contract(new SpringMvcContract())
-                .encoder(new JacksonEncoder())
-                .decoder(new JacksonDecoder())
-                .target(LessonClient.class, address);
+        return createClient(LessonClient.class);
     }
 
     @Bean
     public PersonClient getPersonClient() {
-        return Feign.builder()
-                .contract(new SpringMvcContract())
-                .encoder(new JacksonEncoder())
-                .decoder(new JacksonDecoder())
-                .target(PersonClient.class, address);
+        return createClient(PersonClient.class);
     }
 
     @Bean
     public CourseClient getCourseClient() {
-        return Feign.builder()
-                .contract(new SpringMvcContract())
-                .encoder(new JacksonEncoder())
-                .decoder(new JacksonDecoder())
-                .target(CourseClient.class, address);
+        return createClient(CourseClient.class);
     }
 
     @Bean
     public StreamClient getStreamClient() {
-        return Feign.builder()
-                .contract(new SpringMvcContract())
-                .encoder(new JacksonEncoder())
-                .decoder(new JacksonDecoder())
-                .target(StreamClient.class, address);
+        return createClient(StreamClient.class);
     }
 
     @Bean
     public StudentClient getStudentClient() {
+        return createClient(StudentClient.class);
+    }
+
+    private <T> T createClient(Class<T> type) {
         return Feign.builder()
                 .contract(new SpringMvcContract())
                 .encoder(new JacksonEncoder())
                 .decoder(new JacksonDecoder())
-                .target(StudentClient.class, address);
+                .logger(new Slf4jLogger(type))
+                .logLevel(Logger.Level.FULL)
+                .target(type, address);
     }
-
-//    public static <T> T createClient(Class<T> type, String uri) {
-//        return Feign.builder()
-//                .contract(new SpringMvcContract())
-//                .encoder(new JacksonEncoder())
-//                .decoder(new JacksonDecoder())
-//                .logger(new Slf4jLogger(type))
-//                .logLevel(Logger.Level.FULL)
-//                .target(type, uri);
-//    }
 
 }
---
+++ src/main/java/com/iteco/dp/domain/configuration/FeignClientConfig.java
@@ -0,0 +1,9 @@
+package com.iteco.dp.domain.configuration;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class FeignClientConfig {
+
+
+}
